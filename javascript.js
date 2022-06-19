6ce3f2f69f6f0542df539316a8ca7a2865deb43c
jshapter <j.a.shapter@gmail.com>
2022-06-19 16:29:45 +0100

    Mostly functional

--- javascript.js
+++ javascript.js
@@ -1,21 +1,71 @@
-const grid = document.getElementById('grid-container');
-let pixels = [];
+let grid = document.getElementById('grid-container');
+let square = 16;
+let cellArr = [];
+let colours = 'black'
 
-
-for (let i = 0; i < 256; i++) {
-    let pixel = document.createElement('div');
-    pixels.push(pixel);
-    pixel.classList.add('cell');
-    pixel.id = i;
-    grid.appendChild(pixel);
+function canvas(dim) {
+    for (let i = 0; i < dim * dim; i++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('cell');
+        pixel.id = i;
+        cellArr.push(i);
+        grid.appendChild(pixel);
+    }
+    console.log(cellArr)
+    let setColumns = 'auto '.repeat(square);
+    grid.style.gridTemplateColumns = setColumns;
+    draw(colours);
 }
 
-let setColumns = 'auto '.repeat(Math.sqrt(pixels.length));
-grid.style.gridTemplateColumns = setColumns;
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = "black";
+function draw(colours) {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = colours;
+        });
+    const btn = document.querySelector('#clear');
+    btn.addEventListener('click', () => {
+        cell.style.backgroundColor = "white";
     });
-});
+    });
+}
+
+function x16() {
+    for (let x = 0; x < cellArr.length; x++) {
+        let delCell = document.getElementById(x);
+        delCell.remove();
+    }
+    square = 16;
+    cellArr = []
+    canvas(square);
+}
+
+function x32() {
+    for (let j = 0; j < cellArr.length; j++) {
+        let delCell = document.getElementById(j);
+        delCell.remove();
+    }
+    square = 32;
+    cellArr = []
+    canvas(square);
+}
+
+function x64() {
+    for (let j = 0; j < cellArr.length; j++) {
+        let delCell = document.getElementById(j);
+        delCell.remove();
+    }
+    square = 64;
+    cellArr = []
+    canvas(square);
+}
+
+function colour(clickID) {
+    console.log(colours)
+    colours = clickID
+    console.log(colours)
+    draw(colours);
+}
+
+console.log(colours)
+canvas(square)
